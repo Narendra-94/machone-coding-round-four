6b6ceeb6aa684fc31c2d0861de4b4d16d39c79cc
Narendra <narenchordiya07@gmail.com>
2023-06-24 21:59:07 +0530

    feat:done home-section

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import { SinglePage } from "./components/pages/SinglePage";
+import { Navbar } from "./components/Navbar/Navbar";
+import { NavbarMenu } from "./components/NavbarMenu/NavbarMenu";
+import { SortMenu } from "./components/SortMenu/SortMenu";
+import { Home } from "./components/pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="App-main-page-content">
+        <NavbarMenu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/single-post" element={<SinglePage />} />
+        </Routes>
+        <SortMenu />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ForumProvider } from "./context.js/ForumContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <ForumProvider>
+        <App />
+      </ForumProvider>
+    </Router>
+  </React.StrictMode>
+);
