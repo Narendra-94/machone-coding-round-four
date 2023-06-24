b22adc91baa0fc405b96e1067c6ef1f444d24a8d
Narendra <narenchordiya07@gmail.com>
2023-06-24 22:22:33 +0530

    feat:done sort

--- src/components/SortMenu/SortMenu.js
+++ src/components/SortMenu/SortMenu.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
+import { ForumContext } from "../../context.js/ForumContext";
 
 export const SortMenu = () => {
-  return <div className="sort-menu">SortMenu</div>;
+  const { dispatch } = useContext(ForumContext);
+
+  const handleSortChange = (event) => {
+    const selectedOption = event.target.value;
+    if (selectedOption === "date") {
+      dispatch({ type: "SORT_BY_DATE_CREATED" });
+    } else if (selectedOption === "upvotes") {
+      dispatch({ type: "SORT_BY_UPVOTES" });
+    }
+  };
+
+  return (
+    <div>
+      <select onChange={handleSortChange}>
+        <option value="date">Latest Posts</option>
+        <option value="upvotes">Most Upvoted</option>
+      </select>
+    </div>
+  );
 };
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -7,7 +7,24 @@ export const reducer = (state, action) => {
     case "FETCH_SUCCESSFULL_DATA": {
       return { ...state, forumData: action.payload };
     }
-
+    case "SORT_BY_DATE_CREATED": {
+      const sortedByDate = [...state.forumData.posts].sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+      return {
+        ...state,
+        forumData: { ...state.forumData, posts: sortedByDate },
+      };
+    }
+    case "SORT_BY_UPVOTES": {
+      const sortedByUpvotes = [...state.forumData.posts].sort(
+        (a, b) => b.upvotes - a.upvotes
+      );
+      return {
+        ...state,
+        forumData: { ...state.forumData, posts: sortedByUpvotes },
+      };
+    }
     default:
       return state;
   }
